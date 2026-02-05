9e6bdf8e3864ee7fc328a13397ed00b91394f579
fayjulislambappi <fayjul15-14059@diu.edu.bd>
2026-02-05 18:02:00 +0600

    Allow image quality 80 in next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  images: {
+    qualities: [75, 80, 100],
+  },
 };
 
 export default nextConfig;
