f272daa4a3c85b07214eed89a48e88f6dcd7e222
fayjulislambappi <fayjul15-14059@diu.edu.bd>
2026-02-05 18:37:55 +0600

    Move spider cursor behind main content

--- src/components/SpiderCursor.tsx
+++ src/components/SpiderCursor.tsx
@@ -144,7 +144,7 @@ export default function SpiderCursor() {
                 width: '100%',
                 height: '100%',
                 pointerEvents: 'none',
-                zIndex: 9999, // Overlay everything
+                zIndex: 0, // Behind content
                 background: 'transparent'
             }}
         />
